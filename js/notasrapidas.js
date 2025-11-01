ee32ae2de5d4ddfb55461332ade2bacfd9735fdf
Helena Hagsma <helenahagsma2024@gmail.com>
2025-10-31 23:15:52 -0300

    Atualizar o notasrapidas.js
    debug

--- js/notasrapidas.js
+++ js/notasrapidas.js
@@ -5,25 +5,7 @@ function adicionar(){ /*adicionar nota */
     var naourgente = document.getElementById("naourgente")
     var espacoNU = document.getElementById("espacoNU")
     var espacoNNU = document.getElementById("espacoNNU")
-
-    if(urgente.checked==true && naourgente.checked==false){
-        var li = document.createElement("li");
-        li.textContent = n; /*conteúdo da li será a próxima nota do usuário */
-        li.style.color="#ffffff" /*Visivelmente agradável no campo vermelho */
-    }
-
-    if(naourgente.checked==true && urgente.checked==false){
-        var li = document.createElement("li");
-        li.textContent = n;
-    }    
-    /*adiciona na lista correspondente sem substituir valor*/
-    if (urgente.checked===true) {
-        espacoNU.appendChild(li);
-    }
-    else if (naourgente.checked===true) {
-        espacoNNU.appendChild(li);
-    }
-
+
     /*verificações---------------------------*/
     if(n==""){
         alert("O campo não pode ficar vazio!")
@@ -40,6 +22,24 @@ function adicionar(){ /*adicionar nota */
         return;
     }
     /*---------------------------------------- */
+
+if(urgente.checked==true && naourgente.checked==false){
+        var li = document.createElement("li");
+        li.textContent = n; /*conteúdo da li será a próxima nota do usuário */
+        li.style.color="#ffffff" /*Visivelmente agradável no campo vermelho */
+    }
+
+    if(naourgente.checked==true && urgente.checked==false){
+        var li = document.createElement("li");
+        li.textContent = n;
+    }    
+    /*adiciona na lista correspondente sem substituir valor*/
+    if (urgente.checked===true) {
+        espacoNU.appendChild(li);
+    }
+    else if (naourgente.checked===true) {
+        espacoNNU.appendChild(li);
+    }
 
     /*Limpa o campo e desmarca os checkboxes*/
     document.getElementById("nota").value = "";
